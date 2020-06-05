9efce92dfe468026a424a5a49b95d716b2ce7be6
sebadiaza <sebastian.diazaran@gmail.com>
2020-06-05 15:13:41 -0400

    Script movmiento y rotacion

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public int speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.up * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= transform.up * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += transform.right * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= transform.right * Time.deltaTime * speed;
+        }
+    }
+}
---
+++ Assets/Scripts/Rotacion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacion : MonoBehaviour
+{
+    public float x = 0;
+    public float y = 0;
+    public float z = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(x * Time.deltaTime, y * Time.deltaTime, z * Time.deltaTime);
+    }
+}
